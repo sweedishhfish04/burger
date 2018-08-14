ecc78e4c6503d01237a46f0ee560c436f4492124
sweedishhfish04 <godziwa4@yahoo.com>
2018-08-13 22:36:56 -0500

    "fixed an issue in server"

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const exphbs  = require('express-handlebars')
 
 const express = require("express");
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
 const bodyParser = require("body-parser");
 const router = require("./controllers/burgers_controller")
 
 var app = express();
 
 var PORT = process.env.PORT || 3000;
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 app.use(bodyParser.json());
 app.use("", router)
 
@@ -16,21 +16,3 @@ app.use("", router)
 app.listen(PORT, function () {
     console.log("App listening on PORT: " + PORT);
 });
-
-/*orm.selectAll().then(function(burgers){
-    console.log(burgers)
-}).catch(function(err){
-    console.error(err.message)
-})
-
-orm.updateOne("hamburger").then(function(){
-    console.log("updated")
-}).catch(function(err){
-    console.error(err.message)
-})
-
-orm.insertOne("bacon burger").then(function(){
-    console.log("inserted")
-}).catch(function(err){
-    console.error(err.message)
-})*/
