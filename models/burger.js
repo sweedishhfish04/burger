0e3ecf9c6afdfaaeca29ebe64edd9f82e2530c46
sweedishhfish04 <godziwa4@yahoo.com>
2018-08-13 22:15:17 -0500

    "added to the addBurger code"

--- models/burger.js
+++ models/burger.js
@@ -1,20 +1,26 @@
 const orm = require('../config/orm');
 
-function getBurgers(req, res, next){
-    orm.selectAll().then(function(burgers){
+function getBurgers(req, res, next) {
+    orm.selectAll().then(function (burgers) {
         console.log(burgers)
         res.send(burgers)
-    }).catch(function(err){
+    }).catch(function (err) {
         console.error(err.message)
         res.status(500).send()
     })
 }
 
-function addBurger(req, res, next){
-
+function addBurger(req, res, next) {
+    orm.insertOne(req.pramas.burgerName).then(function () {
+        console.log("Inserted Burger Name")
+        res.send()
+    }).catch(function (err) {
+        console.error(err.message)
+        res.status(500).send()
+    })
 }
 
-function eatBurger(req, res, next){
+function eatBurger(req, res, next) {
 
 }
 
