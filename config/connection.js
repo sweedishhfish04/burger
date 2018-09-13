4c96e13f308768f84f52b1f3f025c2765e8b0a1e
sweedishhfish04 <godziwa4@yahoo.com>
2018-09-12 20:48:19 -0500

    getting app set up

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,25 @@
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '123',
-    database: 'burgers_db'
-});
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: 'bbj31ma8tye2kagi.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        user: 'vz9pit6hhyen85yr',
+        password: 't6inytmcvg9rdw0s',
+        database: 'ms9yntsqpdr9zmut'
+    });
+};
+
+
+
+//const connection = mysql.createConnection({
+//host: 'localhost',
+//user: 'root',
+//password: '123',
+//  database: 'burgers_db'
+//});
 
 connection.connect();
 
